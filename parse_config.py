e8e26594919d87af4c4cef5ec11728d667d9b2d2
Ramon Iglesias <ramon_igl@icloud.com>
2026-05-03 21:21:09 -0300

    feat(parse_config): parsea entrada (str) para um dict

--- parse_config.py
+++ parse_config.py
@@ -1,14 +1,23 @@
+def parse_to_dict(raw_content: str) -> dict:
+    content_dict = dict(item.split("=") for item in raw_content.split("\n"))
+    return content_dict
+
+
 def load(file: str) -> None:
     try:
         with open(file, "r") as f:
-            print(f.read())
+            raw_content = f.read()
     except FileNotFoundError as e:
         print(e)
     except Exception as e:
         print(e)
 
+    configs = parse_to_dict(raw_content)
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
