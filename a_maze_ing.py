00636b4cd8fded67fdc5ec5e8727399df710383c
boeing474 <pedrorboeing@gmail.com>
2026-05-16 15:39:34 -0300

    parse refatorado para evitar crashes e adicionado verificações

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,17 +1,17 @@
 import sys
 import parse_config
 
-def main():
+def main() -> None:
     if len(sys.argv) != 2:
-        print("Use: a_maze_ing.py <config_file>")
-        sys.exit()
+        print("Usage: python3 a_maze_ing.py <ficheiro_de_configuracao>")
+        sys.exit(1)
 
     file_name = sys.argv[1]
 
-    parse_config.load(file_name)
-
-
-
+    parsed_config = parse_config.load(file_name)
+    
+    print("Configs loaded successfully:")
+    print(parsed_config)
 
 if __name__ == "__main__":
     main()
