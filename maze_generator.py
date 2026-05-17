a16c6439f2a314d354ae6a5de3bb2bb6f6064879
Ramon Iglesias <ramon_igl@icloud.com>
2026-05-17 15:02:01 -0300

    feat: prototipo das classes mazeGenerator e Cell criadas

---
+++ maze_generator.py
@@ -0,0 +1,23 @@
+class MazeGenerator:
+    def __init__(self):
+        pass
+        
+    def generate_grid(self, qnt_cells):
+        self.cells = []
+        for cell in range(qnt_cells):
+            self.cells.append(Cell())
+
+        for cell in range(len(self.cells)):
+            self.cells[cell].show_cell()
+
+
+class Cell:
+    def __init__(self):
+        self.visited = False
+        self.north = 0
+        self.east = 0
+        self.south = 0
+        self.west = 0
+
+    def show_cell(self):
+        print(f"{self.north}{self.east}{self.south}{self.west}")
